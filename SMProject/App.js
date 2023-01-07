b850950b6826a85f8a6a7e3b478cf3f89c8f4d22
Arikost <arikostrovsky7@gmail.com>
2023-01-07 18:29:59 +0200

    create navigation menu

---
+++ SMProject/App.js
@@ -0,0 +1,53 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useState} from 'react';
+
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import HomeScreen from './components/pages/HomeScreen';
+import SelectShoes from './components/pages/SelectShoes';
+import { StyleSheet, Text, View, Pressable, SafeAreaView, Button} from "react-native";
+import SelectShirts from './components/pages/SelectShirts';
+import SelectPants from './components/pages/SelectPants';
+import Success from './components/pages/Success';
+
+const Drawer = createDrawerNavigator();
+
+const App = () => {
+
+  return (
+      <Drawer.Navigator>
+        <Drawer.Screen name='Application' component={HomeScreen}/>
+        <Drawer.Screen name='SelectShoes' component={SelectShoes}/>
+        <Drawer.Screen name='SelectPants' component={SelectPants}/>
+        <Drawer.Screen name='SelectShirts' component={SelectShirts}/>
+        <Drawer.Screen name='Success' component={Success}/>
+      </Drawer.Navigator>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: "#2EBC4A"
+  },
+  title: {
+    marginTop: 16,
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: "#20232a",
+    borderRadius: 6,
+    backgroundColor: "#61dafb",
+    color: "#20232a",
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold"
+  }
+});
+
+export default App;
